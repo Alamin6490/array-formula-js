fc33219c81b572c4b24961f3061dfc02267fb394
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-13 22:14:45 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,29 @@
+const ages = [];
+const numbers = [1,87,98,45]
+
+console.log(numbers);
+//how to push in array
+ numbers.push(44);
+ numbers.push(44,65,76);
+console.log(numbers);
+
+// another example
+
+const friends = ['balam','kalam', 'salam', 'pappu', 'munim'];
+//friend .push ('kailam');
+console.log(friends)
+
+
+// how to pop in array
+
+friends.pop();
+console.log(friends);
+friends.pop();
+console.log(friends)
+
+// another one pop example
+const out1 = friends.pop();
+console.log(friends);
+const out1 = friends.pop();
+friends.pop();
+console.log(friends)
